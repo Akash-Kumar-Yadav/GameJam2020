2a8a856752544d33833c55efc9a8bde67963047c
Akash-Kumar-Yadav <akashkumaryadav7411@gmail.com>
2020-12-09 02:08:32 +0530

    Drone Controls Finished need to add sound

---
+++ Assets/Scripts/DroneCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneCamera : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float turnSpeed;
+    private float velocity;
+    [SerializeField] Vector2 min;
+    [SerializeField] Vector2 max;
+    [SerializeField] GameObject child;
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if(h !=0 && v != 0) { return; }
+
+        transform.Translate(h * 5 * Time.deltaTime, 0, v * 5 * Time.deltaTime, Space.World);
+        child.transform.localEulerAngles = new Vector3(-5 * v, -5 * h, 0);
+        UPDown();
+    }
+
+    private void UPDown()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float newPos = Mathf.SmoothDamp(transform.position.y, 25, ref velocity, moveSpeed);
+            transform.position = new Vector3(transform.position.x, newPos, transform.position.z);
+
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            float newPos = Mathf.SmoothDamp(transform.position.y, 12, ref velocity, moveSpeed);
+            transform.position = new Vector3(transform.position.x, newPos, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/RandomCameraMovement.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCameraMovement : MonoBehaviour
+{
+    [SerializeField] Vector3 newPos;
+    [SerializeField] Quaternion newRot;
+   
+    [SerializeField] Vector2 min;
+    [SerializeField] Vector2 max;
+    [SerializeField] float lerpSpeed;
+
+    private void Start()
+    {
+        newPos = transform.position;
+        newRot = transform.rotation;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, newPos, lerpSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRot, lerpSpeed);
+
+        if (Vector3.Distance(transform.position, newPos) < 1)
+        {
+            GetNewPos();
+        }
+    }
+
+    private void GetNewPos()
+    {
+        var x = Random.Range(min.x, max.x);
+        var y = Random.Range(min.y, max.y);
+
+       // newRot = Quaternion.Euler(0, Random.Range(1, 5), 0);
+        newPos = new Vector3(x, 15, y);
+    }
+}
