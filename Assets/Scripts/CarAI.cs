42a0debbe8f640a14c432d9ca987ca12bf6ce724
Akash-Kumar-Yadav <akashkumaryadav7411@gmail.com>
2020-12-09 21:29:29 +0530

    CarAi Done

---
+++ Assets/Scripts/CarAI.cs
@@ -0,0 +1,19 @@
+﻿using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAI : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public EndOfPathInstruction end;
+    public float speed;
+    float dstTravelled;
+    void Update()
+    {
+        dstTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(dstTravelled, end);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(dstTravelled, end);
+    }
+
+}
---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,39 @@
+﻿using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] Prefabs;
+    [SerializeField] float spawnTime;
+    [SerializeField] float numberOfCars;
+    [SerializeField] PathCreator path;
+    int i = 1;
+    //void Update()
+    //{
+    //    if(Time.time > spawnTime)
+    //    {
+    //        GameObject obj = Instantiate(Prefabs[Random.Range(0, Prefabs.Length)], transform.position, Quaternion.identity);
+    //        obj.GetComponent<CarAI>().pathCreator = path;
+    //        spawnTime = Time.time + spawnTime;
+    //    }
+    //}
+
+    private void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    IEnumerator Spawner()
+    {
+        GameObject obj = Instantiate(Prefabs[Random.Range(0, Prefabs.Length)], transform.position, Quaternion.identity);
+        obj.GetComponent<CarAI>().pathCreator = path;
+        yield return new WaitForSeconds(spawnTime);
+        i++;
+        if (i < numberOfCars)
+        {
+            StartCoroutine(Spawner());
+        }
+    }
+}
