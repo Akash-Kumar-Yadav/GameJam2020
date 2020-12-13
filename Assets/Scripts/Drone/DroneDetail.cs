440eaed676de04d52e682f187e7beb3ce45d08a8
Akash-Kumar-Yadav <akashkumaryadav7411@gmail.com>
2020-12-13 12:46:10 +0530

    Ui timer and time and date updated

---
+++ Assets/Scripts/Drone/DroneDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DroneDetail : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] TMP_Text sliderText;
+    [SerializeField] float maxDistance;
+
+  
+    void Update()
+    {
+        RaycastHit info;
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (Physics.Raycast(ray,out info ,1000))
+        {
+            slider.value = info.distance/maxDistance;
+            sliderText.text = (int)info.distance + " M";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TMP_Text timerText;
+    [SerializeField] TMP_Text timeText;
+    [SerializeField] float timer = 300;
+    
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        float min = Mathf.FloorToInt(timer / 60);
+        float sec = Mathf.FloorToInt(timer % 60);
+        timeText.text = DateTime.Now.ToString();
+        timerText.text = string.Format("{0:00}:{1:00}",min,sec);
+    }
+}
